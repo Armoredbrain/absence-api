555679c7febbbff24783fb9dc3d7c15064857c2d
tardigrade <benjamin.jaud@neomanis.io>
2023-06-15 13:58:43 +0200

    :fire: clean up old logic

--- src/controllers/calendar.ts
+++ src/controllers/calendar.ts
@@ -1,32 +1,18 @@
 import { Request, Response } from "express";
 import logger, { ApiError } from "../console/logger";
-import { Year, getGregorianCalendar, getYearIndex, intersectRange } from "../managers/main";
+import { splitAbsenceByMonth, Range } from "../managers/main";
 interface DefaultApiResponse {
     code: string | number;
     message: string;
 }
-// CONTROLLER
 export async function getAbsenceMapping(
     req: Request,
-    res: Response<Year | DefaultApiResponse, Record<string, unknown>>
+    res: Response<Range[] | DefaultApiResponse, Record<string, unknown>>
 ): Promise<void> {
     try {
-        // extract timezone ??
-
-        // input date iso with timezone
         const start = new Date(String(req.query.start));
         const end = new Date(String(req.query.end));
-        // check year range
-        // build calendarChain(s) : calendar 2023 + calendar 2024
-        const calendarChain: Year = {};
-        for (const year of Array<number>(...new Set([getYearIndex(start), getYearIndex(end)]))) {
-            Object.assign(calendarChain, { [year]: getGregorianCalendar(year) });
-        }
-        // intersect absence and yearChain into result -> fun but how
-        const absenceSplitByMonth = intersectRange({ start, end }, calendarChain);
-        // apply timezone ??
-
-        // output calendar object with date iso string mapping
+        const absenceSplitByMonth = splitAbsenceByMonth({ start, end });
         res.status(200).json(absenceSplitByMonth);
     } catch (error) {
         logger.error(
--- src/tests/mock.test.ts
+++
@@ -1,5 +0,0 @@
-describe("TO BE REMOVED", () => {
-    test("should pass", () => {
-        expect(("b" + "a" + +"a" + "a").toLowerCase()).toEqual("banana");
-    });
-});
