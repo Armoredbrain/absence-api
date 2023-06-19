8ebfc35fbb2572d9ab9d8d47d8c38d9b54f2038d
tardigrade <benjamin.jaud@neomanis.io>
2023-06-19 09:31:12 +0200

    :fire: remove expection since not applicable anymore

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -1,7 +1,6 @@
 import { validationResult } from "express-validator";
 import { Response, Request, NextFunction } from "express";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function validator(req: Request, res: Response, next: NextFunction): void {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
