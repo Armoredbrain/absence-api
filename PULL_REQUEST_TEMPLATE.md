4d03a0eed8f981d65cbb97221a52274094958ec9
tardigrade <benjamin.jaud@neomanis.io>
2023-06-19 09:50:27 +0200

    :wrench: modify configuration file relative to git CRLF and jest module mapping

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -9,5 +9,4 @@
 -   [ ] tests and coverage ok
 -   [ ] update README.md
 -   [ ] update .env.example
--   [ ] tests feature with brand new .env (`cp .env.example .env`) and fully up environment
 
--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,6 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    moduleNameMapper: {
-        "test/(.*)": "<rootDir>/test/$1",
-    },
     resetMocks: true,
     collectCoverageFrom: ["**/src/controllers/*.ts", "**/src/managers/**/*.ts", "**/src/middlewares/*.ts"],
 };
